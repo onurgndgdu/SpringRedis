56d57b489815a5726ab998972f012603ad8e3ea6
gundogduonur <gundogduonur@outlook.com.tr>
2023-09-23 16:14:06 +0300

    using redishash on user class

---
+++ src/main/java/com/onurgundogdu/springredis/model/User.java
@@ -0,0 +1,39 @@
+package com.onurgundogdu.springredis.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@RedisHash("User")
+public class User implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
